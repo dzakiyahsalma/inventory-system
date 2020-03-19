6b88251e62e9883163ebb091189a0ea6ba496202
dzakiyahsalma <57974613+dzakiyahsalma@users.noreply.github.com>
2020-03-19 12:34:44 +0700

    Update register.php

--- register.php
+++ register.php
@@ -19,7 +19,11 @@ include('config.php')
         <?php include('error.php'); ?>
         <div class="input-group">
             <label>Username</label>
-            <input type="text" name="username" value="<?php echo $username; ?>">
+            <input type="text" name="username" value="<?php echo $username ?>">
+        </div>
+        <div class="input-group">
+            <label>Nama</label>
+            <input type="text" name="nama">
         </div>
         <div class="input-group">
             <label>Email</label>
@@ -30,8 +34,15 @@ include('config.php')
             <input type="password" name="password_1">
         </div>
         <div class="input-group">
-            <label>Password</label>
+            <label>Confirm Password</label>
             <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+            <label for="level">Jenis User</label>
+            <select id="level" name="level">
+                <option value="Admin">Admin</option>
+                <option value="User">User</option>
+            </select>
         </div>
         <div class="input-group">
             <button type="submit" class="btn" name="reg_user">Sign Up</button>
